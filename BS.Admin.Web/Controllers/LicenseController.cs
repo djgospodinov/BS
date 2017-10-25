55d4603de5573e3bcf84d95b3830dc2662540cb0
Dian Gospodinov <dian.gospodinov@pariplaydev.com>
2017-10-25 16:37:14 +0300

    ui changes

--- BS.Admin.Web/Controllers/LicenseController.cs
+++ BS.Admin.Web/Controllers/LicenseController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BS.Common.Models;
 
 namespace BS.Admin.Web.Controllers
 {
@@ -51,14 +52,58 @@ namespace BS.Admin.Web.Controllers
                 .ToList());
         }
 
+        [HttpGet]
         public ActionResult Create() 
         {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Create(LicenseModel model)
+        {
+            model.User = new LicenserInfoModel();
+            model.User.CompanyId = "123";
+            model.User.Name = "123";
+            model.User.Email = "123";
+            model.User.Phone = "123";
+            model.User.ConactPerson = "123";
+
+            model.Modules = new List<LicenseModulesEnum>();
+            model.Modules.Add(LicenseModulesEnum.Accounting);
+
+            //if (ModelState.IsValid)
+            //{
+                var id = _service.Create(model);
+                if (!string.IsNullOrEmpty(id))
+                {
+                    ViewBag.Message = "Успешно създаден!";
+
+                    var result = _service.Get(id);
+                    return View(result);
+                }
+            //}
+
+            ViewBag.Message = "Възникна грешка!";
+            return View(model);
+        }
+
         public ActionResult Users() 
         {
             return View();
+        }
+
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return Content("Not found.");
+            }
+
+            var result = _service.Get(id);
+            if (result == null)
+                throw new ArgumentException("License for id not found", id);
+
+            return View(result);
         }
     }
 }
