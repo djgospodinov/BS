fdfa7a989eb71d6aacfc12d1727dbbf4ebee6e66
Dian Gospodinov <djgospodinov@gmail.com>
2017-11-04 22:45:48 +0200

    added dropdownhelper class

---
+++ BS.Admin.Web/DropdownHelper.cs
@@ -0,0 +1,38 @@
+﻿using BS.Common.Interfaces;
+using BS.Common.Models;
+using BS.LicenseServer.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BS.Common;
+
+namespace BS.Admin.Web
+{
+    public class DropdownHelper
+    {
+        protected static readonly List<LicenseTypeEnum> _types = new List<LicenseTypeEnum>() 
+            {
+                LicenseTypeEnum.PerComputer,
+                LicenseTypeEnum.PerUser,
+                LicenseTypeEnum.PerServer
+            };
+
+        private static readonly IUserService _service = new UserService();
+
+        public static SelectList LicenseTypes() 
+        {
+            var result = new SelectList(_types.Select(x => new { Id = (int)x, Name = x.Description() }).ToList(), "Id", "Name");
+
+            return result;
+        }
+
+        public static IEnumerable<SelectListItem> LicenseUsers()
+        {
+            var result = new SelectList(_service.GetAll(), "Id", "Name");
+
+            return result;
+        }
+    }
+}
