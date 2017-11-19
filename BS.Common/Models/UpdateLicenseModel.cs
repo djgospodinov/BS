e8161bbe301a9a82b98f141c3794b671f54211f9
Dian Gospodinov <djgospodinov@gmail.com>
2017-11-19 10:57:13 +0200

    ui changes

---
+++ BS.Common/Models/UpdateLicenseModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BS.Common.Models
+{
+     [DataContract]
+    public class UpdateLicenseModel
+    {
+        [DataMember]
+        public DateTime? ValidTo { get; set; }
+
+        [DataMember]
+        public DateTime? SubscribedTo { get; set; }
+
+        [DataMember]
+        public bool? IsDemo { get; set; }
+
+        [DataMember]
+        public int? UserId { get; set; }
+
+        [DataMember]
+        public List<LicenseModulesEnum> Modules { get; set; }
+
+        [DataMember]
+        public LicenseTypeEnum? Type { get; set; }
+
+        [DataMember]
+        public bool? Enabled { get; set; }
+
+        [DataMember]
+        public string ActivationId { get; set; }
+
+        [DataMember]
+        public bool? IsActivated { get; set; }
+    }
+}
