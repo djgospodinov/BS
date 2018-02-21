5b807fad00fd8498a636ce9f9325b4e5c11c049b
Dian Gospodinov <dian.gospodinov@pariplaydev.com>
2018-02-21 11:10:16 +0200

    fix: 1.       Когато създам нова фирма и не попълня ЕГН ми позволява да запиша (При положение, че съм избрал фирма), но след това гърми на редакция. „Input string was not in a correct format.“ Влязох през базата и добавих стойност в ЕГН и се оправи.
    2.       След създаването на нова фирма (Брейн Сторм Консулт Груп АД) влизам в меню Лицензи и създавам лиценз за тази фирма, но не се появява. Видях в базата, че се създава лиценз, който е Enabled- False. Ръчно го правя True, но виждам, че в същност лиценза се създава отново за фирма Брейн Сторм Консулт, а не за Груп АД

---
+++ BS.Admin.Web/Filters/RangeIfAttribute.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BS.Admin.Web.Filters
+{
+    public class RangeIfAttribute : RangeAttribute
+    {
+        public string DependentProperty { get; private set; }
+        public object TargetValue { get; private set; }
+
+        public RangeIfAttribute(string dependentProperty, object targetValue, int minimum, int maximum)
+            : base(minimum, maximum)
+        {
+            DependentProperty = dependentProperty;
+            TargetValue = targetValue;
+        }
+
+        public RangeIfAttribute(string dependentProperty, object targetValue, long minimum, long maximum)
+            : base(minimum, maximum)
+        {
+            DependentProperty = dependentProperty;
+            TargetValue = targetValue;
+        }
+
+        public RangeIfAttribute(string dependentProperty, object targetValue, double minimum, double maximum)
+            : base(minimum, maximum)
+        {
+            DependentProperty = dependentProperty;
+            TargetValue = targetValue;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var property = validationContext.ObjectType.GetProperty(DependentProperty);
+            if (property == null)
+            {
+                return new ValidationResult(string.Format("Unknown property: {0}", DependentProperty));
+            }
+            var otherValue = property.GetValue(validationContext.ObjectInstance, null);
+
+            if (object.Equals(otherValue, TargetValue) && !IsValid(value))
+            {
+                return new ValidationResult("Range");
+            }
+
+            return null;
+        }
+
+        public override bool IsValid(object value)
+        {
+            if (value == null || string.IsNullOrWhiteSpace(value.ToString()))
+            {
+                return false;
+            }
+
+            var length = value.ToString().Length;
+            var minLength = base.Minimum.ToString().Length;
+            var maxLength = base.Maximum.ToString().Length;
+            if (length > maxLength || length < minLength)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
