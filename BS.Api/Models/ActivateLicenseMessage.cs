5c2c7015999eb958095bc97bec932afd9453d6af
Dian Gospodinov <dian.gospodinov@pariplaydev.com>
2018-04-24 09:36:36 +0300

    fix

---
+++ BS.Api/Models/ActivateLicenseMessage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BS.Api.Models
+{
+    /// <summary>
+    /// Отговор към заявката за лицензиране
+    /// </summary>
+    public class ActivateLicenseMessage
+    {
+        /// <summary>
+        /// Идентификатор за който е лицензиран
+        /// </summary>
+        public Guid LicenseId { get; internal set; }
+        public string ActivationKey { get; set; }
+        /// <summary>
+        /// Името на работната станция, където е лицензиран
+        /// </summary>
+        public string ComputerName { get; set; }
+        /// <summary>
+        /// Сървъра, на който ще се използва Мираж
+        /// </summary>
+        public string ServerName { get; set; }
+        /// <summary>
+        /// Статус на активацията
+        /// </summary>
+        public bool Activated { get; internal set; }
+    }
+}
